f10464ecfc389cc1ac807a417c03881e3eca62e9
Leroy <harr2512@umn.edu>
2023-01-25 21:44:42 -0600

    Figure out objects a little more

--- jayess.js
+++ jayess.js
@@ -1,24 +1,25 @@
 lemonkey = 33;
 
 var rock = {
-    name: "rock",
-    beats: [scissors],
-    loses_to: [paper]
+    "name": "rock",
+    "beats": ["scissors"],
+    "loses_to": ["paper"]
 };
 
 var paper = {
-    name: "paper",
-    beats: [rock],
-    loses_to: [scissors]
+    "name": "paper",
+    "beats": ["rock"],
+    "loses_to": ["scissors"]
 };
 
 var scissors = {
-    name: "scissors",
-    beats: [paper],
-    loses_to: [rock]
+    "name": "scissors",
+    "beats": ["paper"],
+    "loses_to": ["rock"]
 };
 
 var choices = [rock, paper, scissors];
+
 
 function getObj (name) {
     for (var ele of choices) {
@@ -30,15 +31,20 @@ function getObj (name) {
 }
 
 function officiate(pc, pcc) {
-    if (pc == pcc) {return `DRAW: ${pc.name} cannay best ${pcc.name}. HARR HARR HARR`}
+    if (pc == pcc) {
+        return {message : `DRAW: ${pc.name} cannay best ${pcc.name}. HARR HARR HARR`,
+                outcome : "T"};
+    }
     
-    for (var ele of pc.beats) {
-        if (ele == pcc) {
-            return `VICTORY: ${pc.name} beats ${pcc.name}`
+    for (ele of pc.beats) {
+        if (ele == pcc.name) {
+            return {message : `VICTORY: ${pc.name} beats ${pcc.name}`,
+                    outcome : "V" };
         }
     }
 
-    return `DEFEAT: ${pcc.name} beats ${pc.name}`
+    return {message : `DEFEAT: ${pcc.name} beats ${pc.name}`,
+            outcome : "D"}
 }
 
 function getComputerChoice() {
@@ -56,5 +62,23 @@ function playRound() {
     pc = getPlayerChoice();
     pcc = getComputerChoice();
 
-    return officiate(pc, pcc);
+    
+    game =  officiate(pc, pcc);
+    console.log(game.message);
+    return game.outcome;
+}
+
+function playGame() {
+    wins = 0;
+    losses = 0;
+
+    for (let i = 0; i < 5; i++) {
+        outcome = playRound();
+        if (outcome == "V") {wins++;}
+        if (outcome == "D") {losses++;}
+    }
+
+    if(wins == losses){return "It's a tie!";}
+    else if(wins > losses){return "You win!";}
+    else {return "You lose!";}
 }
